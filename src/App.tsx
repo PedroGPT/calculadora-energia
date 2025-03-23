e92b416ea7abf0e8f8a02298612c69a808cf9d6e
PedroGPT <actions@github.com>
2025-03-23 21:55:48 +0000

    Mostrar CalculadoraOmip directamente en la raíz

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CalculadoraOmip from "./CalculadoraOmip";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/calculadora-omip" element={<CalculadoraOmip />} />
-      </Routes>
-    </Router>
-  );
+  return <CalculadoraOmip />;
 }
